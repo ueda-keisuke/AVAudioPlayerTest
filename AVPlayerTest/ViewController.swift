ae44c051679412c02ba229086954ee1a8edf9d62
Keisuke Ueda <keisuke@bromantech.com>
2016-04-05 21:42:11 -0700

    Initial Commit

---
+++ AVPlayerTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AVPlayerTest
+//
+//  Created by Neuro Leap on 2016/04/05.
+//  Copyright © 2016年 Neuro Leap. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
